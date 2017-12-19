e158930dcafa5220a7182b2b70a36d88a60870fd
liorg <liorgish@gmail.com>
2017-12-19 09:05:11 +0200

    Add project files.

---
+++ WebApi.Template1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace WebApi.Template1.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            //var file = System.Web.Hosting.HostingEnvironment.MapPath("~/a.mp4");
+            //var arr = File.ReadAllBytes(file);
+            //return Ok(arr);
+
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("~/a.mp4");
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType =
+                new MediaTypeHeaderValue("application/octet-stream");
+            return result;
+        }
+    }
+}
